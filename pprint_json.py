9ff7db9687a4d2c2319773abcecdb5c48a2d8e42
jackssn <cryisis@mail.ru>
2016-09-27 23:19:37 +0300

    'тест'

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,21 @@
 import json
+import os
+from pprint import pprint
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as f:
+        return json.load(f)
 
 
 def pretty_print_json(data):
-    pass
+    return pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    #filepath = input('Type full path to json-file:\n')
+    filepath = '../drinks.json'
+    data = load_data(filepath)
+    print(pretty_print_json(data))
